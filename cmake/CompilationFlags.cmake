447b11579f5ee845b6a0bd389ce425d64ac0bdb5
Nauris Linde <naurislinde@gmail.com>
2023-08-11 09:29:51 +0000

    Fixed compilation on MacOS

--- cmake/CompilationFlags.cmake
+++ cmake/CompilationFlags.cmake
@@ -32,5 +32,5 @@ if(MSVC)
 endif()
 
 if (APPLE)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++${CMAKE_CXX_STANDARD}")
+  add_definitions("-stdlib=libc++ -std=c++${CMAKE_CXX_STANDARD}")
 endif()
