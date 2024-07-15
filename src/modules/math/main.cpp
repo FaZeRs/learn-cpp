fc902119f4c6b970277e367c9704d2bb74f75602
Nauris Linde <naurislinde@gmail.com>
2024-07-15 22:54:59 +0300

    C++ Modules

---
+++ src/modules/math/main.cpp
@@ -0,0 +1,11 @@
+import math; // Import the math module
+
+#include <iostream>
+
+int main() {
+  int a = 5;
+  int b = 3;
+  std::cout << "Add: " << add(a, b) << "\n";
+  std::cout << "Subtract: " << subtract(a, b) << "\n";
+  return 0;
+}
---
+++ src/modules/math/math.cpp
@@ -0,0 +1,5 @@
+export module math;
+
+export int add(int a, int b) { return a + b; }
+
+export int subtract(int a, int b) { return a - b; }
